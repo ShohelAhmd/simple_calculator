6c67c83acf0da8be580b673fa78127771524ed26
shohel-ahmed <shohelahmed27.sa@gmail.com>
2025-02-02 19:13:41 +0800

    added substraction

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ var num1= parseFloat (prompt("Enter First Number : "));
 var num2= parseFloat(prompt("Enter Second Number : "));
 
 var result= num1+num2;
-document.write("Sum = "+result);
+document.write("Sum = "+result+"<br>");
+
+var result= num1 - num2;
+document.write("Sub = "+result);
